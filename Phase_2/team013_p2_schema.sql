8f135d847c7082b357059ca46100a59ff782977a
Ashok Kumar <ashok@qlikpass.com>
2019-06-25 11:56:45 -0500

    added alter statements to include FK

--- Phase_2/team013_p2_schema.sql
+++ Phase_2/team013_p2_schema.sql
@@ -16,29 +16,30 @@ GRANT ALL PRIVILEGES ON `cs6400_sm19_team013`.* TO 'gatechUser'@'localhost';
 FLUSH PRIVILEGES;
 
 -- Tables
+-- Should we start the table names with capitals? 
 
-CREATE TABLE `user` (
+CREATE TABLE `User` (
   login_username varchar(20) NOT NULL,
   login_password varchar(20) NOT NULL,
   user_first_name varchar(50) NOT NULL,
   user_last_name varchar(50) NOT NULL,
-  role varchar(25) NOT NULL, -- should we limit the size of role to a single character? 
+  user_role varchar(25) NOT NULL, -- should we limit the size of role to a single character? Also, change in relationship mapping 
   PRIMARY KEY (login_username)
 );
 
-CREATE TABLE `customer` (
+CREATE TABLE Customer (
   customer_id int(32) unsigned NOT NULL AUTO_INCREMENT,
   phone_number int(10) unsigned NOT NULL, -- Should be varchar, They advised against phone number being 10 digits it. e.g phone numbers with extension
   email varchar(50) NOT NULL,
   street varchar(50) NOT NULL,
   city varchar(50) NOT NULL,
-  state varchar(50) NOT NULL,
+  state_name varchar(50) NOT NULL,
   postal_code int(10) NOT NULL, -- should be varchar
   PRIMARY KEY (customer_id),
   UNIQUE KEY (email)
 );
 
-CREATE TABLE `individual` (
+CREATE TABLE Individual (
   driver_license_number varchar(50) NOT NULL,
   customer_id int(32) unsigned NOT NULL,
   individual_first_name varchar(50) NOT NULL,
@@ -47,7 +48,7 @@ CREATE TABLE `individual` (
   UNIQUE KEY (customer_id)
 );
 
-CREATE TABLE `business` (
+CREATE TABLE Business (
   tax_id_number varchar(50) NOT NULL,
   customer_id int(32) unsigned NOT NULL,
   business_name varchar(50) NOT NULL,
@@ -57,7 +58,7 @@ CREATE TABLE `business` (
   UNIQUE KEY (customer_id)
 );
 
-CREATE TABLE `sale` (
+CREATE TABLE Sale (
   vin varchar(50) NOT NULL,
   customer_id int(32) unsigned NOT NULL,
   login_username varchar(50) NOT NULL,
@@ -65,7 +66,7 @@ CREATE TABLE `sale` (
   PRIMARY KEY (vin)
 );
 
-CREATE TABLE `purchase` (
+CREATE TABLE Purchase (
   vin varchar(50) NOT NULL,
   customer_id int(32) unsigned NOT NULL,
   login_username varchar(50) NOT NULL,
@@ -73,63 +74,87 @@ CREATE TABLE `purchase` (
   PRIMARY KEY (vin)
 );
 
--- TODO left off with Vehicle
-CREATE TABLE `vehicle` (
+CREATE TABLE Vehicle (
   vin varchar(50) NOT NULL,
   manufacturer_name varchar(50) NOT NULL,
-  type_name varchar(50) NOT NULL,
-  model_year smallint NOT NULL,
+  vehicle_type varchar(50) NOT NULL,
+  model_year int(11) NOT NULL,
   model_name varchar(50) NOT NULL,
   mileage float(8) NOT NULL,
   vehicle_condition varchar(10) NOT NULL,
-  description varchar(200) NOT NULL,
+  vehicle_description varchar(200) NOT NULL, -- change in relationship mapping 
   sales_price float(8) NOT NULL,
   kelly_blue_book_value float(8) NOT NULL,
   PRIMARY KEY (vin)
 );
 
-CREATE TABLE `Vehicle-Color` (
+CREATE TABLE VehicleColor (
   vin varchar(50) NOT NULL,
   color varchar(50) NOT NULL,
-  PRIMARY KEY (vin) -- Need to check
+  PRIMARY KEY (vin, color) -- Need to check
 );
 
-CREATE TABLE `Manufacturer` (
+CREATE TABLE Manufacturer (
   manufacturer_name varchar(50) NOT NULL,
   PRIMARY KEY (manufacturer_name)
 );
 
-CREATE TABLE `VehicleType` (
-  type_name varchar(50) NOT NULL,
+CREATE TABLE VehicleType (
+  vehicle_type varchar(50) NOT NULL,
   PRIMARY KEY (type_name)
 );
 
-CREATE TABLE `Repair` (
+CREATE TABLE Repair (
   vin varchar(50) NOT NULL,
-  start_date date NOT NULL,
-  end_date date NOT NULL,
+  repair_start_date date NOT NULL, -- change in relationship mapping 
+  repair_end_date date NOT NULL,
   vendor_name varchar(50) NOT NULL,
   nhtsa_number varchar(50) NULL,
   total_cost float(8) NOT NULL,
   repair_description varchar(200) NOT NULL, -- need to check if this can be NULL
-  status varchar(1) NOT NULL,
-  PRIMARY KEY (vin) -- need to check 
+  vehicle_status varchar(1) NOT NULL, -- change in relationship mapping 
+  PRIMARY KEY (vin, repair_start_date) -- need to check 
 );
 
-CREATE TABLE `Vendor` (
+CREATE TABLE Vendor (
   vendor_name varchar(50) NOT NULL,
   vendor_phone_number varchar(20) NOT NULL,
   street varchar(50) NOT NULL,
   city varchar(50) NOT NULL,
-  state varchar(50) NOT NULL,
+  state_name varchar(50) NOT NULL,
   postal_code varchar(10) NOT NULL, -- should be varchar
   PRIMARY KEY (vendor_name)
 );
 
-CREATE TABLE `Recall` (
+CREATE TABLE Recall (
   nhtsa_number varchar(50) NOT NULL, --change in relationship mapping 
   manufacturer_name varchar(50) NOT NULL,
   recall_description varchar(200) NOT NULL, --change in relationship mapping 
   PRIMARY KEY (nhtsa_number)
 );
 
+-- Constraints   Foreign Keys: FK_ChildTable_childColumn_ParentTable_parentColumn
+
+ALTER TABLE Individual
+  ADD CONSTRAINT fk_Individual_customer_id_Customer_customer_id FOREIGN KEY (customer_id) REFERENCES Customer (customer_id);
+  
+ALTER TABLE Business
+  ADD CONSTRAINT fk_Business_customer_id_Customer_customer_id FOREIGN KEY (customer_id) REFERENCES Customer (customer_id);
+
+ALTER TABLE Sale
+  ADD CONSTRAINT fk_Sale_customer_id_Customer_customer_id FOREIGN KEY (customer_id) REFERENCES Customer (customer_id);
+  ADD CONSTRAINT fk_Sale_login_username_User_login_username FOREIGN KEY (login_username) REFERENCES User (login_username);
+  ADD CONSTRAINT fk_Sale_vin_Vehicle_vin FOREIGN KEY (vin) REFERENCES Vehicle (vin);
+
+ALTER TABLE Purchase
+  ADD CONSTRAINT fk_Purchase_customer_id_Customer_customer_id FOREIGN KEY (customer_id) REFERENCES Customer (customer_id);
+  ADD CONSTRAINT fk_Purchase_login_username_User_login_username FOREIGN KEY (login_username) REFERENCES User (login_username);
+  ADD CONSTRAINT fk_Purchase_vin_Vehicle_vin FOREIGN KEY (vin) REFERENCES Vehicle (vin);
+
+ALTER TABLE VehicleColor
+  ADD CONSTRAINT fk_VehicleColor_vin_Vehicle_vin FOREIGN KEY (vin) REFERENCES Vehicle (vin);
+
+ALTER TABLE Repair
+  ADD CONSTRAINT fk_Repair_vin_Vehicle_vin FOREIGN KEY (vin) REFERENCES Vehicle (vin);
+  ADD CONSTRAINT fk_Repair_vin_Vendor_vendor_name FOREIGN KEY (vendor_name) REFERENCES Vendor (vendor_name);
+  ADD CONSTRAINT fk_Repair_nhtsa_number_Recall_nhtsa_number FOREIGN KEY (nhtsa_number) REFERENCES Recall (nhtsa_number);
